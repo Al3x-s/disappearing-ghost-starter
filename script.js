8d91a6a141a8c8b27093dc043c68cec8b72120d7
Glitch (honored-triangular-lightning) <none>
2020-12-01 22:47:49 +0000

    🎽💶 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+$(".hide-button").click(function() { 
+  $(".gostimg").hide();
+});
 
+$(".show-button").click(function() { 
+  $(".gostimg").show();
+});
+
+$(".update-img-button").click(function() { 
+  $(".gostimg").html('<img src="https://cdn.glitch.com/7980a3ca-c419-4b67-863a-21392b291806%2F59-598130_transparent-cute-ghost-png-sticker-tumblr-transparent-black.png?v=1605656578560">');
+});
+
+var str = 
+$(".message-button").click(function() {
+  $(".message").append(`<p></p>`);
+});
+//almost done 
+
+$(".name-button").click(function() { 
+  $("h1").html('<h1> My name is alex <h1>');
+});
